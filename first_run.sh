af66544f2afc53f166958829d19e7d67f20014a7
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-01-23 12:22:41 +0700

    fix freeipa

--- first_run.sh
+++ first_run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 source .env
 
-docker-compose run --rm --service-ports freeipa exit-on-finished
+#docker-compose run --rm --service-ports freeipa exit-on-finished
+
+docker run --name ${CONTAINER_NAME_FREEIPA} -ti -rm \
+    -h ${FREEIPA_HOST} \
+    -e PASSWORD=${FREEIPA_PASS} \
+    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+    --tmpfs /run --tmpfs /tmp \
+    -v ${SERVICE_DATA}:/data:Z freeipa-server exit-on-finished -U -r ${FREEIPA_REALM} --no-ntp
