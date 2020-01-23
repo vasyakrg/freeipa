d1bec4564a8438600c274c08151eb8d138304faf
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-01-23 12:23:54 +0700

    fix err

--- first_run.sh
+++ first_run.sh
@@ -3,7 +3,7 @@ source .env
 
 #docker-compose run --rm --service-ports freeipa exit-on-finished
 
-docker run --name ${CONTAINER_NAME_FREEIPA} -ti -rm \
+docker run --name ${CONTAINER_NAME_FREEIPA} -ti --rm \
     -h ${FREEIPA_HOST} \
     -e PASSWORD=${FREEIPA_PASS} \
     -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
